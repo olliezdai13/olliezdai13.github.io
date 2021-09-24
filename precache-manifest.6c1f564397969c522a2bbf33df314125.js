78719bbd90eb95136c57cad5228906ad1e91d825
Oliver <olliezdai@gmail.com>
2021-09-24 12:54:19 -0400

    Updates

---
+++ precache-manifest.6c1f564397969c522a2bbf33df314125.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3c88b8dd23c346ba97b4e5cf9474e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f11ff3f4de2b77e2b8e2",
+    "url": "/static/css/2.0c1563c7.chunk.css"
+  },
+  {
+    "revision": "48aeee9214cdc8a3bfcb",
+    "url": "/static/css/main.a64bcba2.chunk.css"
+  },
+  {
+    "revision": "f11ff3f4de2b77e2b8e2",
+    "url": "/static/js/2.c7a31074.chunk.js"
+  },
+  {
+    "revision": "48aeee9214cdc8a3bfcb",
+    "url": "/static/js/main.62ac87f2.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "06b528621d135871fdd9cdc988916970",
+    "url": "/static/media/advancedgrails.06b52862.svg"
+  },
+  {
+    "revision": "e1490625d159ac8303c1cace0bad1904",
+    "url": "/static/media/documentation.e1490625.svg"
+  },
+  {
+    "revision": "b651640f09f462efc43058482f4131d4",
+    "url": "/static/media/grails-cupsonly-logo-white.b651640f.svg"
+  },
+  {
+    "revision": "cda5b2716e249b1f09558c5c3aa79ddb",
+    "url": "/static/media/grails.cda5b271.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07688f36a61209ddace7da11c5eabdd4",
+    "url": "/static/media/slack.07688f36.svg"
+  }
+]);
